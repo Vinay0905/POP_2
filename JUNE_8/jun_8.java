2e0e2ffb07ba6acf9dca8676e89c7664a87e068d
Naga Vinay Avvaru <nagavinay0905@gmail.com>
2026-06-09 11:41:51 +0530

    compplete bst

--- JUNE_8/jun_8.java
+++ JUNE_8/jun_8.java
@@ -1,166 +1,166 @@
-package JUNE_8;
+// package JUNE_8;
 
-public class jun_8 {
-    
-
-}
-
-class Solution {
-    public void solve(int n,int op,int cl,String ans,List<String> res)
-    {
-        if(op==n && cl==n)
-        {
-          res.add(ans); 
-          return ;
-        }
-        else 
-        {
-          if(op<n)
-          {
-            solve(n,op+1,cl,ans+"(",res);
-          }
-          if(op>cl)
-          {
-            solve(n,op,cl+1,ans+")",res);
-          }
-        }
-    }
-    public List<String> generateParenthesis(int n)
-    {
-        List<String> res = new ArrayList<>(); 
-        int op=0,cl=0; 
-        String ans=""; 
-        solve(n,op,cl,ans,res); 
-        return res;
-    }
-}
+// import java.util.ArrayList;
+// import java.util.List;
 
 
-class Solution { 
 
-    public List<int[]> merge(List<int[]> left,List<int[]> right) 
-    {
-        List<int[]> res = new ArrayList<>();
-         int i=0,j=0; 
-         int h1=0,h2=0; 
-         int x=0; 
-         while(i<left.size() && j<right.size())
-         {
-           if(left.get(i)[0] < right.get(j)[0])
-           {
-             x = left.get(i)[0]; 
-             h1 = left.get(i)[1]; 
-             i++; 
-           }
-           else if(left.get(i)[0] > right.get(j)[0])
-           {
-             x = right.get(j)[0]; 
-             h2 = right.get(j)[1];
-             j++; 
-           }
-           else 
-           {
-             x = right.get(j)[0];
-             h1 = left.get(i)[1];
-             h2 = right.get(j)[1];
-             i++;
-             j++;
-           }
-           int m = Math.max(h1,h2); 
-           if(res.isEmpty() || res.get(res.size()-1)[1]!=m)
-           {
-            res.add(new int[]{x,m});
-           }
-         }
-         while(i<left.size())
-         {
-           x = left.get(i)[0]; 
-             h1 = left.get(i)[1]; 
-             i++; 
-             int m = Math.max(h1,h2); 
-           if(res.isEmpty() || res.get(res.size()-1)[1]!=m)
-           {
-            res.add(new int[]{x,m});
-           }
-         }
-         while(j<right.size())
-         {
-           x = right.get(j)[0]; 
-             h2 = right.get(j)[1]; 
-             j++; 
-             int m = Math.max(h1,h2); 
-           if(res.isEmpty() || res.get(res.size()-1)[1]!=m)
-           {
-            res.add(new int[]{x,m});
-           }
-         }
-         return res;
-    }
-    public List<int[]> build(int[][] buildings,int start,int end)
-    {//start = 0  end = 5
-        if(start>=end)
-        {
-            return new ArrayList<>();
-        }
-        if(start+1 == end)
-        {
-          List<int[]> res = new ArrayList<>();  
-          res.add(new int[]{buildings[start][0],buildings[start][2]});
-          res.add(new int[]{buildings[start][1],0});
-          return res;
-
-        }
-        int mid = (start+end)/2; //mid=2 ; 
-        List<int[]> left = build(buildings,start,mid);
-        List<int[]> right = build(buildings,mid,end);  
-        return merge(left,right);
+// class Solution {
+//     public void solve(int n,int op,int cl,String ans,List<String> res)
+//     {
+//         if(op==n && cl==n)
+//         {
+//           res.add(ans); 
+//           return ;
+//         }
+//         else 
+//         {
+//           if(op<n)
+//           {
+//             solve(n,op+1,cl,ans+"(",res);
+//           }
+//           if(op>cl)
+//           {
+//             solve(n,op,cl+1,ans+")",res);
+//           }
+//         }
+//     }
+//     public List<String> generateParenthesis(int n)
+//     {
+//         List<String> res = new ArrayList<>(); 
+//         int op=0,cl=0; 
+//         String ans=""; 
+//         solve(n,op,cl,ans,res); 
+//         return res;
+//     }
+// }
 
 
-    }
-    public List<List<Integer>> getSkyline(int[][] buildings) 
-    {
-        int n = buildings.length; 
-        if(n==0)
-        {
-            return new ArrayList<>();
-        } 
-       List<int[]> pairs =  build(buildings,0,n);
-       List<List<Integer>> ans = new ArrayList<>();
-       for(int[] p : pairs)
-       {
-        ans.add(Arrays.asList(p[0],p[1])); 
-       }
-       return  ans;
-    }
-}
+// class jun_8 { 
+
+//     public List<int[]> merge(List<int[]> left,List<int[]> right) 
+//     {
+//         List<int[]> res = new ArrayList<>();
+//          int i=0,j=0; 
+//          int h1=0,h2=0; 
+//          int x=0; 
+//          while(i<left.size() && j<right.size())
+//          {
+//            if(left.get(i)[0] < right.get(j)[0])
+//            {
+//              x = left.get(i)[0]; 
+//              h1 = left.get(i)[1]; 
+//              i++; 
+//            }
+//            else if(left.get(i)[0] > right.get(j)[0])
+//            {
+//              x = right.get(j)[0]; 
+//              h2 = right.get(j)[1];
+//              j++; 
+//            }
+//            else 
+//            {
+//              x = right.get(j)[0];
+//              h1 = left.get(i)[1];
+//              h2 = right.get(j)[1];
+//              i++;
+//              j++;
+//            }
+//            int m = Math.max(h1,h2); 
+//            if(res.isEmpty() || res.get(res.size()-1)[1]!=m)
+//            {
+//             res.add(new int[]{x,m});
+//            }
+//          }
+//          while(i<left.size())
+//          {
+//            x = left.get(i)[0]; 
+//              h1 = left.get(i)[1]; 
+//              i++; 
+//              int m = Math.max(h1,h2); 
+//            if(res.isEmpty() || res.get(res.size()-1)[1]!=m)
+//            {
+//             res.add(new int[]{x,m});
+//            }
+//          }
+//          while(j<right.size())
+//          {
+//            x = right.get(j)[0]; 
+//              h2 = right.get(j)[1]; 
+//              j++; 
+//              int m = Math.max(h1,h2); 
+//            if(res.isEmpty() || res.get(res.size()-1)[1]!=m)
+//            {
+//             res.add(new int[]{x,m});
+//            }
+//          }
+//          return res;
+//     }
+//     public List<int[]> build(int[][] buildings,int start,int end)
+//     {//start = 0  end = 5
+//         if(start>=end)
+//         {
+//             return new ArrayList<>();
+//         }
+//         if(start+1 == end)
+//         {
+//           List<int[]> res = new ArrayList<>();  
+//           res.add(new int[]{buildings[start][0],buildings[start][2]});
+//           res.add(new int[]{buildings[start][1],0});
+//           return res;
+
+//         }
+//         int mid = (start+end)/2; //mid=2 ; 
+//         List<int[]> left = build(buildings,start,mid);
+//         List<int[]> right = build(buildings,mid,end);  
+//         return merge(left,right);
 
 
-class Solution {
-    public void solve(String digits,List<String> res,String[] map,String ans,int i)
-    { // digits="23"  res = [] , ans = "" , i = 0
-        if(i==digits.length())
-        {
-            res.add(ans); 
-            return;
-        }
-        else 
-        {
-            String temp = map[digits.charAt(i)-'0']; //temp = abc; 
-            for(int j=0; j<temp.length();j++)
-            {
-                solve(digits,res,map,ans+temp.charAt(j),i+1);
-            }
-        }
-    }
-    public List<String> letterCombinations(String digits)
-    {//digits = "23" 
-      List<String> res = new ArrayList<>(); 
-      if(digits.length()==0)
-      {
-        return res;
-      }
-      String[] map = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-      solve(digits,res,map,"",0);//functionn calling   
-      return res;
-    }
-}
+//     }
+//     public List<List<Integer>> getSkyline(int[][] buildings) 
+//     {
+//         int n = buildings.length; 
+//         if(n==0)
+//         {
+//             return new ArrayList<>();
+//         } 
+//        List<int[]> pairs =  build(buildings,0,n);
+//        List<List<Integer>> ans = new ArrayList<>();
+//        for(int[] p : pairs)
+//        {
+//         ans.add(Arrays.asList(p[0],p[1])); 
+//        }
+//        return  ans;
+//     }
+// }
+
+
+// class Solution˙ {
+//     public void solve(String digits,List<String> res,String[] map,String ans,int i)
+//     { // digits="23"  res = [] , ans = "" , i = 0
+//         if(i==digits.length())
+//         {
+//             res.add(ans); 
+//             return;
+//         }
+//         else 
+//         {
+//             String temp = map[digits.charAt(i)-'0']; //temp = abc; 
+//             for(int j=0; j<temp.length();j++)
+//             {
+//                 solve(digits,res,map,ans+temp.charAt(j),i+1);
+//             }
+//         }
+//     }
+//     public List<String> letterCombinations(String digits)
+//     {//digits = "23" 
+//       List<String> res = new ArrayList<>(); 
+//       if(digits.length()==0)
+//       {
+//         return res;
+//       }
+//       String[] map = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+//       solve(digits,res,map,"",0);//functionn calling   
+//       return res;
+//     }
+// }
--- JUNE_9/BST_INtro.java
+++ JUNE_9/BST_INtro.java
@@ -17,7 +17,7 @@ class BST{
     BST(){
         root=null;
     }
-
+    //Insert---------------------------------------------------------------------------------------------------------
     public void insert(int val){
         root=insertNode(root,val);
     }
@@ -36,7 +36,10 @@ class BST{
         return root;
     }
 
+
     
+
+    //Search---------------------------------------------------------------------------------------------------------
     boolean search(int val){
         return searchRec(root,val);
     }
@@ -50,6 +53,8 @@ class BST{
         if(val<root.val)return searchRec(root.left, val);
         else return searchRec(root.right, val);
     }
+
+    //Inorder------------------------------------------------------------------------------------------------------------------------------
     void inorder(){
         inorderRec(root);
     }
@@ -61,6 +66,80 @@ class BST{
         System.out.println(root.val+" ");
         inorderRec(root.right);
     }
+    
+    private int inorderPre(Node temp){
+        while(temp.right!=null){
+            temp=temp.right;
+
+        }
+        return temp.val;
+    }
+    public Integer getInorderPre(int val){
+        Node curr=root;
+        while(curr!=null && curr.val!=val){
+            if(val<curr.val){
+                curr=curr.left;
+            }
+            else{
+                curr=curr.right;
+            }
+        }
+        if(curr==null || curr.left==null){
+            return null;
+        }
+        return inorderPre(curr.left);
+    }
+    private int inorderSuc(Node temp){
+        while(temp.left!=null){
+            temp=temp.left;
+
+        }
+        return temp.val;
+    }
+    //Deletion---------------------------------------------------------------------------------------------------------
+    void delete(int val){
+        root=deleteNode(root,val);
+
+    }
+    Node deleteNode(Node root, int val){
+        if(root==null)return null;
+        if(val<root.val)root.left=deleteNode(root.left, val);
+        else if(val>root.val)root.right=deleteNode(root.right, val);
+        else{
+            //3 types :- 
+                // 1) left side no child 
+                // 2) right side no child 
+                // 3) no child atall
+            if(root.left==null )return root.right;
+            if (root.right==null)return root.left;
+            //changes the curr val with the inordersuccessor value
+            root.val=inorderSuc(root.right);
+            // now the inorderSucc value is deleted.
+            root.right=deleteNode(root.right, root.val);
+        }
+        return root;
+
+    }
+    
+    //Validation------------------------------------------------------------------------------------------------------------------------------
+    public boolean isValidBST(Node root) {
+        return isValid(root,null,null) ;
+    }
+    public boolean isValid(Node root,Integer min, Integer max){
+
+        if(root==null){
+            return true;
+        }
+        else if(min!=null && root.val<=min){
+            return false;
+        }
+        else if (max!=null &&root.val>=max){
+            return false;
+        }
+        return isValid(root.left, min, root.val) && isValid(root.right, root.val, max);
+    }
+
+
 
 }
 
@@ -68,10 +147,50 @@ class BST{
 public class BST_INtro {
     public static void main(String[] args) {
         BST tree=new BST();
-        tree.insert(100);
-        tree.insert(200);
-        tree.insert(98);
-        tree.insert(32);
+
+        System.out.println("Inserting values...");
+        int[] values = {100, 50, 200, 25, 75, 150, 250, 60, 80};
+
+        for (int val : values) {
+            tree.insert(val);
+        }
+
+        System.out.println("\nInorder traversal after insertion:");
+        tree.inorder();
+
+        System.out.println("\nSearching values:");
+        System.out.println("Search 75: " + tree.search(75));
+        System.out.println("Search 999: " + tree.search(999));
+        System.out.println("Search 100: " + tree.search(100));
+
+        System.out.println("\nChecking if tree is valid BST:");
+        System.out.println(tree.isValidBST(tree.root));
+
+        System.out.println("\nChecking inorder predecessor:");
+        System.out.println("Inorder predecessor of 100: " + tree.getInorderPre(100));
+        System.out.println("Inorder predecessor of 75: " + tree.getInorderPre(75));
+        System.out.println("Inorder predecessor of 25: " + tree.getInorderPre(25));
+        System.out.println("Inorder predecessor of 999: " + tree.getInorderPre(999));
+
+        System.out.println("\nDeleting leaf node 25...");
+        tree.delete(25);
+        tree.inorder();
+        System.out.println("Valid BST: " + tree.isValidBST(tree.root));
+
+        System.out.println("\nDeleting node with one child 75...");
+        tree.delete(75);
+        tree.inorder();
+        System.out.println("Valid BST: " + tree.isValidBST(tree.root));
+
+        System.out.println("\nDeleting node with two children 100...");
+        tree.delete(100);
+        tree.inorder();
+        System.out.println("Valid BST: " + tree.isValidBST(tree.root));
+
+        System.out.println("\nDeleting value not present 999...");
+        tree.delete(999);
+        tree.inorder();
+        System.out.println("Valid BST: " + tree.isValidBST(tree.root));
 
     }
 }
