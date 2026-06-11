3bd73ca10b4709a82b57cef8929e7efbcb3f9d89
Naga Vinay Avvaru <nagavinay0905@gmail.com>
2026-06-11 14:45:58 +0530

    new

--- JUNE_11/LongestSubstringWithoutRepeatingCharacters.java
+++ JUNE_11/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,28 +1,33 @@
 package JUNE_11;
 
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.HashMap;
+// import java.util.HashSet;
+// import java.util.LinkedList;
+// import java.util.Queue;
 
 public class LongestSubstringWithoutRepeatingCharacters {
     
 
     public static int lengthOfLongestSubstring(String s) {
-        HashSet<Character> set=new HashSet<>();
-        Queue<Character> q=new LinkedList<>();
-        int maxLen=0;
-        for(char ch:s.toCharArray()){
-            while(set.contains(ch)==true){
-                set.remove(q.poll());
-            }
-            q.offer(ch);
-            set.add(ch);
-            maxLen=Math.max(maxLen, q.size());
+    HashMap<Character, Integer> map = new HashMap<>();
+
+    int left = 0;
+    int maxLen = 0;
+
+    for (int right = 0; right < s.length(); right++) {
+        char ch = s.charAt(right);
+
+        if (map.containsKey(ch)) {
+            left = Math.max(left, map.get(ch) + 1);
         }
-        return maxLen;
 
+        map.put(ch, right);
 
+        maxLen = Math.max(maxLen, right - left + 1);
     }
+
+    return maxLen;
+}
     public static void main(String[] args) {
         String s = "abcabcbb";
         System.out.println(lengthOfLongestSubstring(s));
